f87897ae4ebb9f8c2a9e840a459dd29d7c0fcb07
Bulat <bulat0talipov@gmail.com>
2024-06-24 18:47:13 +0300

    added page cube

---
+++ src/js/modules/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+export default () => {
+  let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 30, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 30, // Скорость удаления
+    loop: true // Указываем, повторять ли анимацию
+  });
+}
