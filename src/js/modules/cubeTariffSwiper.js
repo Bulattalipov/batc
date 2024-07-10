ad73e5a5b9182a4384b8a4c6e4e454234a7a4aec
gilyazov <gilyazov.aydar@gmail.com>
2024-07-10 16:27:19 +0300

    фиксы ВАТС

--- src/js/modules/cubeTariffSwiper.js
+++ src/js/modules/cubeTariffSwiper.js
@@ -8,14 +8,15 @@ Swiper.use([Navigation, Pagination]);
 export default () => {
   if (window.matchMedia('(max-width: 1199px)').matches) {
     const slider = document.querySelector(".cube-tariff__swiper");
+    const navigationContainer = document.querySelector(".cube-tariff-navigation");
 
     new Swiper(slider, {
       slidesPerView: 1,
       spaceBetween: 20,
       // centeredSlides: true,
       navigation: {
-        nextEl: slider.querySelector('.swiper-navigation__prev'),
-        prevEl: slider.querySelector('.swiper-navigation__next')
+        nextEl: navigationContainer.querySelector('.swiper-navigation__next'),
+        prevEl: navigationContainer.querySelector('.swiper-navigation__prev')
       },
       breakpoints: {
         // when window width is >= 320px
