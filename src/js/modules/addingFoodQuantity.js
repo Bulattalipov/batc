fbee8ca9b39e5cb53dd011df085416f99c0b60dd
gilyazov <gilyazov.aydar@gmail.com>
2024-11-01 18:47:19 +0300

    фиксы

--- src/js/modules/addingFoodQuantity.js
+++ src/js/modules/addingFoodQuantity.js
@@ -9,21 +9,21 @@ export default () => {
     let total = item.querySelector('.cube-tariff__item-price-elem span');
 
     let basePrice = total.textContent;
-    if (item.classList.contains('cube-tariff__item--colm-2')) {
+    /*if (item.classList.contains('cube-tariff__item--colm-2')) {
       basePrice = item.querySelector('.cube-tariff__item-price-crossed span').textContent;
-    }
+    }*/
 
     checkbox.addEventListener('input', () => {
       if (checkbox.checked) {
         total.innerHTML = parseInt(basePrice) + 100;
-        if (item.classList.contains('cube-tariff__item--colm-2')) {
+        /*if (item.classList.contains('cube-tariff__item--colm-2')) {
           item.querySelector('.cube-tariff__item-price-crossed').style.display = 'none';
-        }
+        }*/
       } else {
         total.innerHTML = parseInt(tariffSum);
-        if (item.classList.contains('cube-tariff__item--colm-2')) {
+        /*if (item.classList.contains('cube-tariff__item--colm-2')) {
           item.querySelector('.cube-tariff__item-price-crossed').style.display = 'block';
-        }
+        }*/
       }
     });
   })
